7087c38405320cfe296b9ca985a5d6af174adf84
Luesbelin-Mejia <124405459+LuesMejia@users.noreply.github.com>
2025-08-19 15:19:01 -0600

    tablero seduc

--- src/vistas/SEDUC.jsx
+++ src/vistas/SEDUC.jsx
@@ -4,12 +4,12 @@ import React from "react";
 const PowerBIView = () => {
   return (
     <div style={{ padding: "2rem", display: "flex", justifyContent: "center" }}>
-     
+    
       <iframe
-        title="SIIE"
+        title="SIIE-SEDUC-CONED"
         width="110%"
         height="820"
-        src="https://app.powerbi.com/view?r=eyJrIjoiMjNhMDQ2MzAtMGRlOS00MDllLWExMTktNGM5Yjk4ZDBjMjBhIiwidCI6IjI5MjY3MDJhLWZhZTctNDY5Yi04OWVmLTQwOGY2ZTJkMzliNiJ9&pageName=6c97aac860a3c262c965"
+        src="https://app.powerbi.com/view?r=eyJrIjoiNjJjNmU0NzctOTM5Yi00ZDQzLWIyNjAtMDFiYTA5NGNkOWEwIiwidCI6IjI5MjY3MDJhLWZhZTctNDY5Yi04OWVmLTQwOGY2ZTJkMzliNiJ9"
         frameborder="0"
         allowFullScreen="true"
       ></iframe>
