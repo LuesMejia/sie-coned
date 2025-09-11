73558335773d3568584dfcbd0a0721fae3b82083
Luesbelin-Mejia <124405459+LuesMejia@users.noreply.github.com>
2025-09-10 21:32:05 -0600

    Tablero de inicipo con diseño

--- src/vistas/Inicio.jsx
+++ src/vistas/Inicio.jsx
@@ -8,7 +8,7 @@ const INICIO = () => {
                 title="SIIE TODAS"
                 width="100%"
                 height="820"
-                src="https://app.powerbi.com/view?r=eyJrIjoiM2I5YTljYjItMDY1My00NTdhLWI4YzktMjY2OTRlM2M5ZTJmIiwidCI6IjI5MjY3MDJhLWZhZTctNDY5Yi04OWVmLTQwOGY2ZTJkMzliNiJ9"
+                src="https://app.powerbi.com/view?r=eyJrIjoiZTlhZDg3NDktOWY0NC00ZGIzLWFkYTQtYWUzMDY1ODIyYmZlIiwidCI6IjI5MjY3MDJhLWZhZTctNDY5Yi04OWVmLTQwOGY2ZTJkMzliNiJ9"
                 frameborder="0"
                 allowFullScreen="true">
             </iframe>
