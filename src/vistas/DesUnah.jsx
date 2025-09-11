da1b841675b786f80cf04491198f641804a09f33
Luesbelin-Mejia <124405459+LuesMejia@users.noreply.github.com>
2025-09-11 10:38:24 -0600

    Tablero de des unah con diseño

--- src/vistas/DesUnah.jsx
+++ src/vistas/DesUnah.jsx
@@ -4,13 +4,15 @@ import React from "react";
 const DESUNAH = () => {
     return (
         <div style={{ padding: "2rem", display: "flex", justifyContent: "center" }}>
-            <iframe title="SIIE DES" 
-            width="100%"
-            height="820" 
-            src="https://app.powerbi.com/view?r=eyJrIjoiMTBiZTk3NGQtZWQ4NS00ZTk3LTlkNGQtODIyNWI5Nzg0ZWJlIiwidCI6IjI5MjY3MDJhLWZhZTctNDY5Yi04OWVmLTQwOGY2ZTJkMzliNiJ9" 
-            frameborder="0" 
-            allowFullScreen="true">    
+            <iframe
+                title="SIE-CONDE-DES"
+                width="100%"
+                height="820"
+                src="https://app.powerbi.com/view?r=eyJrIjoiOGExZmViYTEtMjU2NC00YWVmLWI5ZWEtNjhkODY4Mzg2ZjdiIiwidCI6IjI5MjY3MDJhLWZhZTctNDY5Yi04OWVmLTQwOGY2ZTJkMzliNiJ9&pageName=828e7e43b727c16e1792"
+                frameborder="0"
+                allowFullScreen="true">
             </iframe>
+
         </div>
     );
 };
